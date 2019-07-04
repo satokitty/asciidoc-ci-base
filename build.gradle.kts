112a824376c38e555fe2f9fdcd03b835d81179f7
satokitty <tomohiko.s@gmail.com>
2019-07-04 22:10:21 +0900

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.asciidoctor.gradle.jvm.AsciidoctorTask
+
+plugins {
+    id("org.asciidoctor.jvm.base") version "2.0.0"
+    id("org.asciidoctor.jvm.convert") version "2.0.0"
+}
+
+version = "0.0.1"
+
+repositories {
+    mavenLocal()
+    jcenter()
+}
+
+tasks {
+    "asciidoctor"(AsciidoctorTask::class) {
+        asciidoctorj {
+            setDiagramVersion("1.5.16")
+        }
+
+        outputOptions {
+            setBackends(setOf("html5"))
+        }
+        setSourceDir(file("docs"))
+    }
+}
+
+defaultTasks = listOf("clean", "asciidoctor")
